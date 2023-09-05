3d0e607da59a96c57bc6c022d05af793d70d0461
CynGL <cyntialino09@gmail.com>
2023-09-05 20:16:52 -0300

    caio

--- app/src/main/java/br/com/myskout/tablayoutfragment/CachorroFragment.java
+++ app/src/main/java/br/com/myskout/tablayoutfragment/CachorroFragment.java
@@ -7,9 +7,28 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 
 public class CachorroFragment extends Fragment {
+
+    ListView lstChorro;]
+
+    //Criar dados
+    String nomeChorro[] = {"Mike", "Pulga", "Silvio", "Lulu"};
+
+    String descricaoChorro[] = {
+            "Quero ser adotado alguém ai se disponibiliza...",
+            "Alguém gosta de Cachorro feio? estou aqui...",
+            "Gosto de humano, sou muito carinhoso...",
+            "Estou disponível para ser adotado..."
+    };
+    String numChorro[] = {"5.0", "8.0", "9.0", "9.5"};
+    int imgChorro[] = {
+            R.drawable.mike, R.drawable.chorroB,
+            R.drawable.silvio,R.drawable.lulu
+    };
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
